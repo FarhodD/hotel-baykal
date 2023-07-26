b6eb6ff9a5486e6ce92bd75c4dc479212b3336ce
FarhodD <farhoddavlatov0120@gmail.com>
2023-07-26 18:53:06 +0500

    navigate menu, modal window

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+// burger
 const menuBtn = document.querySelector(".burger__menu"),
   submenu = document.querySelector(".menu"),
   burgerClose = document.querySelector(".burger-close");
@@ -6,3 +7,67 @@ function toggleMenu() {
   menuBtn.classList.toggle("opened-burger");
   submenu.classList.toggle("vis__menu");
 }
+
+
+
+// activeLink
+let links = document.querySelectorAll('.menu__link');
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    links.forEach(otherLink => {
+      otherLink.classList.remove('active__link');
+    });
+    link.classList.add('active__link');
+  });
+});
+
+
+// modalWindow
+const modalOverlay = document.querySelector(".modal-overlay");
+const modals = document.querySelectorAll(".modal");
+const modalBtns = document.querySelectorAll(".modalBtn");
+const formBtns = document.querySelectorAll('.form__btn');
+
+modalBtns.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    let path = e.currentTarget.getAttribute("data-path");
+
+    modals.forEach((el) => {
+      el.classList.remove("modal--visible");
+    });
+
+    document
+      .querySelector(`[data-target='${path}']`)
+      .classList.add("modal--visible");
+    modalOverlay.classList.add("modal-overlay--visible");
+  });
+});
+
+modalOverlay.addEventListener("click", (e) => {
+
+  if (e.target == modalOverlay) {
+    modalOverlay.classList.remove("modal-overlay--visible");
+    modals.forEach((el) => {
+      el.classList.remove("modal--visible");
+    });
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.code == "Escape") {
+    modalOverlay.classList.remove("modal-overlay--visible");
+    modals.forEach((el) => {
+      el.classList.remove("modal--visible");
+    });
+  }
+});
+
+// cancelBtn.forEach((canc) => {
+//   canc.addEventListener("click", () => {
+//     modalOverlay.classList.remove("modal-overlay--visible");
+//     modals.forEach((el) => {
+//       el.classList.remove("modal--visible");
+//     });
+//   });
+// });
